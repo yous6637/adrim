7e42c9edfb254e26042c47c812620db0082b7056
youcef <gagiboss60@gmail.com>
2024-08-16 12:43:58 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -15,8 +15,8 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "jsm-x9",
-project: "javascript-nextjs",
+org: "youcef-gagi",
+project: "bankingsystem",
 }, {
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
